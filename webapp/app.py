671471163aa5c687e31cb00580ba37eb239e6ec7
Aditya Thakur <aditya@Adityas-MacBook-Air-2.local>
2026-04-28 08:21:15 +0530

    Final deployment fix

--- webapp/app.py
+++ webapp/app.py
@@ -368,4 +368,6 @@ def server_error(e):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
